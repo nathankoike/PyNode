18eec834fa1417cf6d1f638c8653b896861aeb6e
Khalid Umar <khalid.umar101@gmail.com>
2019-10-01 00:38:01 -0400

    pyscript file

--- pyScript.js
+++ pyScript.js
@@ -16,7 +16,7 @@ const app = express();
 app.listen(3000, console.log("listening on port 3000"));
 
 app.get("/", (req, res) => {
-  // run python script syncronously
+  // run python script synchronously
   execSync("python gen_fil.py", (err, stdout, stderr) => {
     if (err) console.log(err);
   });
